f16286f5352df843dde4ee2deb44cc92105c5178
Sorawit Chuchan <69070204@kmitl.ac.th>
2026-07-17 18:17:30 +0700

    Update saitama.py

--- Another things/saitama.py
+++ Another things/saitama.py
@@ -1,3 +1,4 @@
+"""saitamu"""
 pushup=int(input())
 situp=int(input())
 squat=int(input())
@@ -8,22 +9,22 @@ one_d_run=int(input())
 one_d_squat=int(input())
 day=[]
 #วิดพื้น
-if pushup % one_d_pushup != 0:
+if pushup % one_d_pushup:
     day.append((pushup // one_d_pushup)+1)
 else:
     day.append(pushup // one_d_pushup)
 #ซิทอัพ
-if situp % one_d_situp != 0:
+if situp % one_d_situp:
     day.append((situp // one_d_situp)+1)
 else:
     day.append(situp // one_d_situp)
 #ลุกนั่ง
-if squat % one_d_squat != 0:
+if squat % one_d_squat:
     day.append((squat//one_d_squat)+1)
 else:
     day.append(squat//one_d_squat)
 #วิ่ง
-if run % one_d_run != 0:
+if run % one_d_run:
     day.append((run // one_d_run)+1)
 else:
     day.append(run//one_d_run)
